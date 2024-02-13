d49cabcbe56b1e913cd616a0846ada4087e4123a
Rich Morban <richardmorban21@gmail.com>
2024-02-13 17:36:22 -0500

    Added ExerciseListConverter and @DynamoDBTypeConverted annotation in WorkoutPlan entity

---
+++ src/main/java/com/decadev/converters/ExerciseListConverter.java
@@ -0,0 +1,32 @@
+package com.decadev.converters;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
+import com.decadev.entities.Exercise;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class ExerciseListConverter implements DynamoDBTypeConverter<String, List<Exercise>> {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public String convert(List<Exercise> exercises) {
+        try {
+            return objectMapper.writeValueAsString(exercises);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting list of exercises to JSON string", e);
+        }
+    }
+
+    @Override
+    public List<Exercise> unconvert(String json) {
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<Exercise>>() {});
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON string to list of exercises", e);
+        }
+    }
+}
