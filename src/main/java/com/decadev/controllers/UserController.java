a4b185bd41cbc0ca7f5432b33c41762914b7ee62
Rich Morban <richardmorban21@gmail.com>
2024-02-13 15:48:24 -0500

    Added Logger to createUser method in UserController

--- src/main/java/com/decadev/controllers/UserController.java
+++ src/main/java/com/decadev/controllers/UserController.java
@@ -8,42 +8,44 @@ import com.decadev.services.UserService;
 import com.decadev.utils.EmailValidator;
 import com.decadev.utils.PasswordValidator;
 import com.decadev.utils.UsernameValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody User user) throws UserAlreadyExistsException, UserNotFoundException {
-        System.out.println("1");
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user) throws UserAlreadyExistsException, UserNotFoundException {
         // Validate username using UsernameValidator
         if (!UsernameValidator.isValidUsername(user.getUsername())) {
             return ResponseEntity.badRequest().body("Username must be 6-12 characters long and contain only letters and numbers.");
         }
-        System.out.println("2");
 
         // Validate email using EmailValidator
         if (!EmailValidator.isValidEmail(user.getEmail())) {
             return ResponseEntity.badRequest().body("Invalid email format.");
         }
-        System.out.println("3");
 
         // Validate password using PasswordValidator
         if (!PasswordValidator.isValidPassword(user.getPassword())) {
             return ResponseEntity.badRequest().body("Password must consist of 6-15 characters and include uppercase, lowercase, numbers, and !$* symbols.");
         }
-        System.out.println("4");
 
         userService.createUser(user);
-        return ResponseEntity.ok("User created successfully");
+
+        log.info("Creating a new user with username: {}", user.getUsername());
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
     }
 
     @GetMapping("{userId}")
